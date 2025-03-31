ad51ea3e3247874d4bb32cacff7b0868e2064170
ounjuu <koj1555@naver.com>
2025-03-31 15:44:07 +0900

    Feat: board

--- back-project/src/app.module.ts
+++ back-project/src/app.module.ts
@@ -42,7 +42,7 @@ import { Weather } from './weather/weather.entity';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'korizon5479@@',
+      password: '1234',
       database: 'puppies',
       entities: [
         User,
