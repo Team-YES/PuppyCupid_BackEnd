b7bc7b11d8025f495d2da70c174aa362039d49af
yujeen02 <yujin5479@gmail.com>
2025-04-02 14:57:26 +0900

    feat. 게시물 등록

--- back-project/src/dogs/dogs.service.ts
+++ back-project/src/dogs/dogs.service.ts
@@ -3,14 +3,23 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Dog } from './dogs.entity';
 
+interface CreateInfoInput {
+  userId: number;
+  name: string;
+  age: number;
+  breed: string;
+  mbti?: string;
+  personality: string;
+  dog_image: string;
+  latitude: number;
+  longitude: number;
+  dong_name: string;
+}
+
 @Injectable()
 export class DogsService {
   constructor(
     @InjectRepository(Dog)
     private readonly dogRepository: Repository<Dog>,
   ) {}
-
-  findAllDog() {
-    return this.dogRepository.find();
-  }
 }
--- back-project/src/main.ts
+++ back-project/src/main.ts
@@ -3,9 +3,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads',
+  });
+
   app.use(cookieParser());
   app.enableCors({
     origin: 'http://localhost:3000',
