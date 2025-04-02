6dfdc1b2b3d8944d8c50cea8d36a9f2a7fd471da
yujeen02 <yujin5479@gmail.com>
2025-04-02 20:09:22 +0900

    feat. 내 정보 수정

--- back-project/src/users/users.controller.ts
+++ back-project/src/users/users.controller.ts
@@ -1,14 +1,30 @@
 // users.controller.ts
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Put, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @UseGuards(AuthGuard('jwt'))
-  @Get('me') // GET /users/me
+  @Get('info')
   getProfile(@Req() req: Request) {
-    console.log('요청한 사용자 정보:', req.user);
     return req.user;
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put('update')
+  async updateform(
+    @Req() req: Request,
+    @Body() body: { nickname: string; phone: string },
+  ) {
+    const user = req.user as any;
+    await this.usersService.updateProfile(user.id, {
+      nickName: body.nickname,
+      phone: body.phone,
+    });
+    return { ok: true, message: '정보 수정 완료' };
   }
 }
