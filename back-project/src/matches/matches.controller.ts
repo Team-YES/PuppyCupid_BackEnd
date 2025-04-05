41202bb3fd430fa3ee3b397ea9df32fa02ba5eba
yujeen02 <yujin5479@gmail.com>
2025-04-05 14:57:54 +0900

    feat. 매칭 및 강아지 정보 수정

--- back-project/src/matches/matches.controller.ts
+++ back-project/src/matches/matches.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Get,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { MatchesService } from './matches.service';
+import { AuthGuard } from '@nestjs/passport';
+
+interface MatchRequest {
+  fromDogId: number;
+  toDogId: number;
+}
 
 @Controller('matches')
-export class MatchesController {}
+@UseGuards(AuthGuard('jwt'))
+export class MatchesController {
+  constructor(private readonly matchesService: MatchesService) {}
+
+  @Post('request')
+  async requestMatch(@Body() body: MatchRequest) {
+    const { fromDogId, toDogId } = body;
+    return await this.matchesService.requestMatch(fromDogId, toDogId);
+  }
+  @Post(':matchId/respond')
+  async respondMatch(
+    @Param('matchId') matchId: number,
+    @Body('action') action: 'accept' | 'reject',
+  ) {
+    return await this.matchesService.respondToMatch(matchId, action);
+  }
+
+  @Get('mine/:dogId')
+  async getMyMatches(@Param('dogId') dogId: number) {
+    return await this.matchesService.getMyMatches(dogId);
+  }
+}
--- back-project/src/matches/matches.module.ts
+++ back-project/src/matches/matches.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MatchesController } from './matches.controller';
 import { MatchesService } from './matches.service';
+import { Match } from './matches.entity';
+import { Dog } from 'src/dogs/dogs.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Match, Dog])],
   controllers: [MatchesController],
-  providers: [MatchesService]
+  providers: [MatchesService],
 })
 export class MatchesModule {}
