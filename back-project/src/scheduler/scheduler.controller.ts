57f07b2d7a33d5f04a42c5e506b68f3e7b8fc7ff
ounjuu <koj1555@naver.com>
2025-04-10 17:03:30 +0900

    Feat: 삭제

--- back-project/src/scheduler/scheduler.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { SchedulerService } from './scheduler.service';
-
-@Controller('scheduler')
-export class SchedulerController {
-  constructor(private readonly schedulerService: SchedulerService) {}
-}
