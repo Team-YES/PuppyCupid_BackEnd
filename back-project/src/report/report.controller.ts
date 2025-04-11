3002637e021d806e2f56045250ffbcfbd6ee54b2
yujeen02 <yujin5479@gmail.com>
2025-04-11 16:21:31 +0900

    feat. 오류 수정

--- back-project/src/report/report.controller.ts
+++ back-project/src/report/report.controller.ts
@@ -1,33 +1,36 @@
-import { Controller, Post, Body, UseGuards, Req, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Req,
+  Param,
+  Get,
+} from '@nestjs/common';
 import { ReportsService } from './report.service';
 import { AuthGuard } from '@nestjs/passport';
 import { report_type } from './report.entity';
 import { AuthRequest } from 'src/users/users.controller';
 
-@Controller('reports')
+@Controller('report')
 @UseGuards(AuthGuard('jwt'))
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
-  @Post()
+  @Post(':type/:targetId')
   async reportContent(
     @Req() req: AuthRequest,
-    @Body()
-    body: {
-      reportType: report_type;
-      targetId: number;
-      reason: string;
-    },
+    @Param('type') type: report_type,
+    @Param('targetId') targetId: string,
+    @Body() body: { reason: string },
   ) {
     const user = req.user;
 
-    const { reportType, targetId, reason } = body;
-
     const report = await this.reportsService.createReport(
       user.id,
-      reportType,
-      targetId,
-      reason,
+      type,
+      Number(targetId),
+      body.reason,
     );
     return { ok: true, report };
   }
