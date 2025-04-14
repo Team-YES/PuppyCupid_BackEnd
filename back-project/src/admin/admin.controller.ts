d2517a8a49dd21cd44656bab5e7f099ed5377756
yujeen02 <yujin5479@gmail.com>
2025-04-14 18:58:39 +0900

    feat. 좋아요 페이지

--- back-project/src/admin/admin.controller.ts
+++ back-project/src/admin/admin.controller.ts
@@ -6,24 +6,28 @@ import { InquiryStatus } from 'src/inquiries/inquiries.entity';
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
+  // 모든 유저 정보
   @Get('users')
   async getUsers() {
     const users = await this.adminService.getAllUsers();
     return { ok: true, users };
   }
 
+  // 유저 삭제
   @Delete('users/:id')
   async deleteUser(@Param('id') id: number) {
     const user = await this.adminService.deleteUserAsAdmin(id);
     return { ok: true, user };
   }
 
+  // 모든 신고 내역
   @Get('reports')
   async getReports() {
     const reports = await this.adminService.getAllReports();
     return { ok: true, reports };
   }
 
+  // 모든 문의 내역
   @Get('inquiries')
   async getInquiries() {
     const inquiries = await this.adminService.getAllInquiries();
@@ -42,12 +46,14 @@ export class AdminController {
     return { ok: true, updated };
   }
 
+  // 문의 삭제
   @Delete('inquiries/:id')
   async deleteInquiry(@Param('id') id: number) {
     const result = await this.adminService.deleteInquiry(id);
     return { ok: true, result };
   }
 
+  // 모든 결제 내역
   @Get('payments')
   async getPayments() {
     const payments = await this.adminService.getAllPayments();
