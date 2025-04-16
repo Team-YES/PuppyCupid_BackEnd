29493f875191b5801e894cba97cc8f7a944459eb
yujeen02 <yujin5479@gmail.com>
2025-04-16 16:17:47 +0900

    feat. swagger 하는중

---
+++ back-project/src/admin/dto/updateInquiryStatus.dto.ts
@@ -0,0 +1,13 @@
+import { IsEnum } from 'class-validator';
+import { InquiryStatus } from 'src/inquiries/inquiries.entity';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateInquiryStatusDto {
+  @ApiProperty({
+    example: 'IN_PROGRESS',
+    description: '문의 처리 상태 (PENDING | IN_PROGRESS | RESOLVED)',
+    enum: InquiryStatus,
+  })
+  @IsEnum(InquiryStatus)
+  status: InquiryStatus;
+}
