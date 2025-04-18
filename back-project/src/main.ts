adb94b8c8975e868a91435411f3b48298f005e7d
yujeen02 <yujin5479@gmail.com>
2025-04-18 19:18:17 +0900

    feat. git

--- back-project/src/main.ts
+++ back-project/src/main.ts
@@ -4,6 +4,8 @@ import * as cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
