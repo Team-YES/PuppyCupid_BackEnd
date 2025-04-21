84f7281042634abd750e41c0205a21ca8deb4871
yujeen02 <yujin5479@gmail.com>
2025-04-22 00:30:19 +0900

    feat. crypto

--- back-project/src/main.ts
+++ back-project/src/main.ts
@@ -4,6 +4,7 @@ import * as cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+(global as any).crypto = require('crypto');
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
