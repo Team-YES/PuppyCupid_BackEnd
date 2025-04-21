2df8974cb9033eceb37e21e018abda7d8d80c49e
yujeen02 <yujin5479@gmail.com>
2025-04-21 19:20:15 +0900

    feat. 배포 전 수정

--- back-project/src/app.module.ts
+++ back-project/src/app.module.ts
@@ -34,7 +34,7 @@ import { SchedulerModule } from './scheduler/scheduler.module';
 // typeorm
 import { TypeOrmModule } from '@nestjs/typeorm';
 // config
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 // schedule
 import { ScheduleModule } from '@nestjs/schedule';
 import { Blacklist } from './admin/blacklist.entity';
@@ -43,32 +43,36 @@ import { SeederModule } from './seeder/seeder.module';
 @Module({
   imports: [
     ScheduleModule.forRoot(),
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: +(process.env.DB_PORT || 3306),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [
-        User,
-        Dog,
-        Post,
-        PostImage,
-        Comment,
-        Like,
-        Inquiry,
-        Message,
-        Notification,
-        Follow,
-        Payment,
-        Weather,
-        ChatCondition,
-        Report,
-        Blacklist,
-      ],
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env', }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: parseInt(config.get<string>('DB_PORT')!, 10),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [
+          User,
+          Dog,
+          Post,
+          PostImage,
+          Comment,
+          Like,
+          Inquiry,
+          Message,
+          Notification,
+          Follow,
+          Payment,
+          Weather,
+          ChatCondition,
+          Report,
+          Blacklist,
+        ],
+        synchronize: true,
+      }),
     }),
     AuthModule,
     UsersModule,
