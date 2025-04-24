4b4b5a78dff34d03b727881008b8090f2e0d19f8
yujeen02 <yujin5479@gmail.com>
2025-04-24 11:41:18 +0900

    fix. users 엔티티 수정

--- back-project/src/users/users.entity.ts
+++ back-project/src/users/users.entity.ts
@@ -31,9 +31,6 @@ export class User {
   @Column({ type: 'varchar', length: 50, unique: true })
   email: string;
 
-  @Column({ type: 'varchar', length: 20, nullable: true })
-  name: string | null;
-
   @Column({ type: 'varchar', length: 20, unique: true, nullable: true })
   nickName: string | null;
 
