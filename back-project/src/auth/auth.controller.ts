1f9db576a0a5e140c3459fdce1bde4862f27d7dd
yujeen02 <yujin5479@gmail.com>
2025-04-24 10:24:34 +0900

    fix. 로그인 쿼리스트링

--- back-project/src/auth/auth.controller.ts
+++ back-project/src/auth/auth.controller.ts
@@ -42,9 +42,22 @@ export class AuthController {
     const FRONT_URL = this.configService.get<string>('FRONT_URL')!;
 
     if (!result.ok) {
+      // 전화번호 필요 → 프론트에 임시 토큰과 상태 전달
+      if (result.needPhoneNumber) {
+        return res.redirect(
+          `${FRONT_URL}/phone?temp_access_token=${result.temp_access_token}`,
+        );
+      }
+
+      // 기타 로그인 실패
       return res.redirect(`${FRONT_URL}/login`);
     }
-    return res.redirect(`${FRONT_URL}`);
+
+    // 정상 로그인 → 메인 페이지로 이동
+    const { access_token, refresh_token } = result;
+    return res.redirect(
+      `${FRONT_URL}/?access_token=${access_token}&refresh_token=${refresh_token}`,
+    );
   }
 
   // // 카카오 로그인
@@ -74,7 +87,7 @@ export class AuthController {
     // 정상 로그인 → 메인 페이지로 이동
     const { access_token, refresh_token } = result;
     return res.redirect(
-      `${FRONT_URL}/access_token=${access_token}&refresh_token=${refresh_token}`,
+      `${FRONT_URL}/?access_token=${access_token}&refresh_token=${refresh_token}`,
     );
   }
 
@@ -105,7 +118,7 @@ export class AuthController {
     // 정상 로그인 → 메인 페이지로 이동
     const { access_token, refresh_token } = result;
     return res.redirect(
-      `${FRONT_URL}/access_token=${access_token}&refresh_token=${refresh_token}`,
+      `${FRONT_URL}/?access_token=${access_token}&refresh_token=${refresh_token}`,
     );
   }
 
